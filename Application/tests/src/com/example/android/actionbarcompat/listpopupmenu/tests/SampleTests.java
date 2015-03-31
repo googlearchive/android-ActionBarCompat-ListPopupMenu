a4d5704120281defe295bcf69371c37d6f1a7299
google-automerger <automerger@google.com>
2015-03-30 17:42:19 -0700

    Auto-update

--- Application/tests/src/com/example/android/actionbarcompat/listpopupmenu/tests/SampleTests.java
+++ Application/tests/src/com/example/android/actionbarcompat/listpopupmenu/tests/SampleTests.java
@@ -1,22 +1,4 @@
 /*
-* Copyright 2013 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-
-
-/*
 * Copyright (C) 2013 The Android Open Source Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -43,7 +25,7 @@ import android.test.ActivityInstrumentationTestCase2;
 public class SampleTests extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private MainActivity mTestActivity;
-    private ListPopupMenuFragment mTestFragment;
+    private PopupListFragment mTestFragment;
 
     public SampleTests() {
         super(MainActivity.class);
@@ -58,8 +40,8 @@ public class SampleTests extends ActivityInstrumentationTestCase2<MainActivity> 
         // flags = {@link Intent#FLAG_ACTIVITY_NEW_TASK}
         // All other fields are null or empty.
         mTestActivity = getActivity();
-        mTestFragment = (ListPopupMenuFragment)
-            mTestActivity.getSupportFragmentManager().getFragments().get(1);
+        mTestFragment = (PopupListFragment)
+            mTestActivity.getSupportFragmentManager().getFragments().get(0);
     }
 
     /**
